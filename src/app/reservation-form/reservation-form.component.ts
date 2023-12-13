e6480f7f8a869b07612de672fa96626d5a0e474c
Maria Angelova <angelova.m@hotmail.com>
2023-12-13 12:04:05 +0100

    add validation & validation messages

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
 
-  reservationForm: FormGroup = new FormGroup({})
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+  
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
 
   onSubmit() {
-    
+    if (this.reservationForm.valid) {
+      this.reservationForm = new FormGroup({})
+    }
   }
  }
