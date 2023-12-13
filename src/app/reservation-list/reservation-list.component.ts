0e096132ec5bccbc83c5c3461f49270cf428b048
Maria Angelova <angelova.m@hotmail.com>
2023-12-13 13:45:53 +0100

    add reservation list

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reservation-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  reservations: Reservation[] = [];
+
+  constructor(
+    private reservationService: ReservationService
+  ) {}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getAllReservations()
+  }
+
 
 }
